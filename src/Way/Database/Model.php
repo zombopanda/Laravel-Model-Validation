ca3e7d83cdc07ea995edb950ec8a4886f2c12e3b
zombopanda <zombopanda@gmail.com>
2015-01-25 16:43:23 +0200

    Allow multiple saving events.

--- src/Way/Database/Model.php
+++ src/Way/Database/Model.php
@@ -96,4 +96,38 @@ class Model extends Eloquent {
         return ! empty($this->errors);
     }
 
+    /**
+     * Save the model to the database.
+     *
+     * @param  array  $options
+     * @return bool
+     */
+    public function save(array $options = array())
+    {
+        $query = $this->newQueryWithoutScopes();
+        // If the "saving" event returns false we'll bail out of the save and return
+        // false, indicating that the save failed. This gives an opportunities to
+        // listeners to cancel save operations if validations fail or whatever.
+        if ($this->fireModelEvent('saving', false) === false)
+        {
+            return false;
+        }
+        // If the model already exists in the database we can just update our record
+        // that is already in this database using the current IDs in this "where"
+        // clause to only update this model. Otherwise, we'll just insert them.
+        if ($this->exists)
+        {
+            $saved = $this->performUpdate($query, $options);
+        }
+        // If the model is brand new, we'll insert it into our database and set the
+        // ID attribute on the model to the value of the newly inserted row's ID
+        // which is typically an auto-increment value managed by the database.
+        else
+        {
+            $saved = $this->performInsert($query, $options);
+        }
+        if ($saved) $this->finishSave($options);
+        return $saved;
+    }
+
 }
